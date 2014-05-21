d787a7b13c334cc083a0afe2f8f4cb24ce107e0a
Jorge Aparicio <japaric@linux.com>
2014-05-21 10:55:31 -0500

    013: Solve in Python and Rust

---
+++ problems/013/013.rs
@@ -0,0 +1,48 @@
+extern crate num;
+extern crate test;
+extern crate time;
+
+use num::bigint::BigInt;
+use std::io::fs::File;
+use std::iter::AdditiveIterator;
+use std::os::args;
+use test::black_box;
+use time::precise_time_ns;
+
+#[inline]
+fn f() -> ~str {
+    let content = match File::open(&Path::new("013.txt")) {
+        Err(_) => fail!("couldn't open input file"),
+        Ok(mut file) => match file.read_to_str() {
+            Err(_) => fail!("couldn't read input file"),
+            Ok(string) => string,
+        }
+    };
+
+    let sum = content.lines()
+                     .filter_map(|line| from_str::<BigInt>(line.trim()))
+                     .sum()
+                     .to_str();
+
+    sum.slice(0, 10).to_owned()
+}
+
+fn main() {
+    match args().as_slice() {
+        [_] => {
+            println!("{}", f());
+        },
+        [_, ref iters] => {
+            let iters: u64 = from_str(iters.as_slice()).unwrap();
+
+            let start = precise_time_ns();
+            for _ in range(0, iters) {
+                black_box(f());
+            }
+            let end = precise_time_ns();
+
+            println!("{}", end - start);
+        },
+        _ => unreachable!(),
+    }
+}
---
+++ problems/013/README.md
@@ -0,0 +1,3 @@
+# [Large sum](https://projecteuler.net/problem=13)
+
+![](https://raw.githubusercontent.com/japaric/eulermark.rs/master/plots/013.png)
